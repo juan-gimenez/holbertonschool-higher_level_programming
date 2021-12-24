9c999f7ed9e15aca30332890f077ba56e2a88941
Juan Gimenez <juancitogimenez0@gmail.com>
2021-12-23 16:17:30 -0800

    task 2 pep8 check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
     else:
-        print("{:d} arguments.".format(len(sys.argv) - 1))        
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     if len(sys.argv) >= 2:
         for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))

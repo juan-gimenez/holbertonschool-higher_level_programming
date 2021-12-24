a89fba5ca6ada1dfa83ab53accaa787ae542ed40
Juan Gimenez <juancitogimenez0@gmail.com>
2021-12-23 16:11:43 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     if len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
-    elif len(sys.argv) > 2:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
     else:
         print("{:d} arguments.".format(len(sys.argv) - 1))
         if len(sys.argv) >= 2:

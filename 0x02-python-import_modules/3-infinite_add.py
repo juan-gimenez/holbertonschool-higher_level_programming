8df09f8c4232ca37c4192ebda55c2b9622dcb5b7
Juan Gimenez <juancitogimenez0@gmail.com>
2021-12-23 16:38:26 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
+import sys
 if __name__ == "__main__":
-    for name in dir(hidden_4):
-        if name[:2] != '__':
-        print(name)
+    add = 0
+    for i in range(1, len(sys.argv)):
+        add += int(sys.argv[i])
+    print("{:d}".format(add))

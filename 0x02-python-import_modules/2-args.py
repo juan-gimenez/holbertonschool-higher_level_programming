fc1e619879cce5db2c04bc63c498a9b4dece1b74
Juan Gimenez <juancitogimenez0@gmail.com>
2021-12-23 16:14:14 -0800

    task 2 for added

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
         print("{:d} argument:".format(len(sys.argv) - 1))
     else:
         print("{:d} arguments.".format(len(sys.argv) - 1))        
+    if len(sys.argv) >= 2:
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
+        i += 1

62ba396e96f620ff5d00507134cd99ecfcee1b42
juan-gimenez <juancitogimenez0@gmail.com>
2022-01-04 13:31:14 -0800

    pycode fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-     nlist = ""
-     nlist = dict(a_dictionary)
-     for x in nlist:
-         nlist[x] = a_dictionary[x] * 2
-     return listnew
+    nlist = ""
+    nlist = dict(a_dictionary)
+    for x in nlist:
+        nlist[x] = a_dictionary[x] * 2
+    return nlist

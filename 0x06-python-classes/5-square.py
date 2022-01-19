e3706c02354ca241c55266ef446255ff5a76f20d
juan-gimenez <juancitogimenez0@gmail.com>
2022-01-18 16:48:29 -0800

    error fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,27 +7,27 @@ class Square:
     def __init__(self, size=0):
         self.size = size
 
-        @origin
-        def size(self):
-            """size origin"""
-            return self.__size
+    @origin
+    def size(self):
+        """size origin"""
+        return self.__size
 
-        @size.set
-        def size(self, value):
-            """sett to size"""
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+    @size.set
+    def size(self, value):
+        """sett to size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-        def area(self):
-            return(self.__size ** 2)
+    def area(self):
+        return(self.__size ** 2)
 
-        def my_print(self):
-            if self.__size == 0:
-                print()
-            else:
-                for x in range(self.__size):
-                    print("#" * self.__size)
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__size):
+                print("#" * self.__size)

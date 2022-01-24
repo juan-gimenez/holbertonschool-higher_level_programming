7c147b4d550a3dc18aac2b1e8e6b827fa8ea07e5
juan-gimenez <juancitogimenez0@gmail.com>
2022-01-24 13:32:38 -0800

    from task0 to 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+task 0
+"""
+
+
+class Rectangle:
+    """class Rectangle"""
+    pass
+
+
+def __init__(self, width=0, height=0):
+    """ attrib """
+    if type(width) is not int:
+        raise TypeError("width must be an integer")
+    if width < 0:
+        raise ValueError("width must be >= 0")
+    if type(height) is not int:
+        raise TypeError("height must be an integer")
+    if height < 0:
+        raise ValueError("height must be >= 0")
+    self.__height = height
+    self.__width = width
+
+
+@property
+def height(self):
+    """ property height"""
+    return self.__height
+
+
+@height.setter
+def height(self, value):
+    """ set the height"""
+    if type(value) is not int:
+        raise TypeError("height must be an integer")
+    if value < 0:
+        raise ValueError("height must be >= 0")
+    self.__height = value
+
+
+@property
+def width(self):
+    """property width"""
+    return self.__width
+
+
+@width.setter
+def width(self, value):
+    """set the widht"""
+    if type(value) != int:
+        raise TypeError("must be an integer")
+    if value < 0:
+        raise ValueError("must be >= 0")
+    self.__width = value
---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+task 0
+"""
+
+
+class Rectangle:
+    """qwdawedempty class Rectangle"""
+    pass

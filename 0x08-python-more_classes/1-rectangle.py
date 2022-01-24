e65b7bf4e8d4818676909887689e281fc8cf5bfe
juan-gimenez <juancitogimenez0@gmail.com>
2022-01-24 14:52:20 -0800

    changed conditions

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,55 +1,42 @@
 #!/usr/bin/python3
-"""
-task 0
-"""
+"""Class Rectangle"""
 
 
 class Rectangle:
-    """class Rectangle"""
-    pass
+    """Class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Init Rectangle"""
+        if (isinstance(width, int) is False):
+            raise TypeError("width must be an integer")
+        if (width < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        if (isinstance(height, int) is False):
+            raise TypeError("height must be an integer")
+        if (height < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = height
 
+    @property
+    def width(self):
+        return self.__width
 
-def __init__(self, width=0, height=0):
-    """ attrib """
-    if type(width) is not int:
-        raise TypeError("width must be an integer")
-    if width < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = width
-    if type(height) is not int:
-        raise TypeError("height must be an integer")
-    if height < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = height
+    @width.setter
+    def width(self, value):
+        if (isinstance(value, int) is False):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
+        @property
+        def height(self):
+            return self.__height
 
-@property
-def height(self):
-    """ property height"""
-    return self.__height
-
-
-@height.setter
-def height(self, value):
-    """ set the height"""
-    if type(value) is not int:
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = value
-
-
-@property
-def width(self):
-    """property width"""
-    return self.__width
-
-
-@width.setter
-def width(self, value):
-    """set the widht"""
-    if type(value) != int:
-        raise TypeError("must be an integer")
-    if value < 0:
-        raise ValueError("must be >= 0")
-    self.__width = value
+        @height.setter
+        def height(self, value):
+            if (isinstance(value, int) is False):
+                raise TypeError("height must be an integer")
+            if (value < 0):
+                raise ValueError("height must be >= 0")
+            self.__height = value

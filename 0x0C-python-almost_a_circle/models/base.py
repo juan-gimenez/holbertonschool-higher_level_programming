791503040742120a32e3e3b4ad0ed4db841b4826
juan-gimenez <https://github.com/juan-gimenez>
2022-02-07 17:38:05 -0800

    base updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,3 +38,28 @@ class Base:
         else:
             with open(op, 'w+') as f:
                 f.write(self.to_json_string(list()))
+
+    def from_json_string(json_string):
+        """JSON string representation"""
+        if json_string is None:
+            return []
+        if len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    def create(cls, **dictionary):
+        """Returns an inst"""
+        if cls.__name__ == "Square":
+            d = cls(1)
+        elif cls.__name__ == "Rectangle":
+            d = cls(1, 1)
+            d.update(**dictionary)
+            return d
+
+    def save_to_file_csv(cls, list_objs):
+        """Saves to csv """
+        r = [item.to_dictionary() for item in list_objs]
+        with open(cls.__name__ + ".csv", mode="w") as save_fcsv:
+            write_csv = csv.DictWriter(save_fcsv, r[0].keys())
+            write_csv.writeheader()
+            write_csv.writerows(r)

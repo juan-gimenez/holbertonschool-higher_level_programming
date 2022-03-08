b8b02cd38f17e7c187c84dedd76404f5ff08ae15
juan-gimenez <juancitogimenez0@gmail.com>
2022-03-08 13:36:03 -0800

    task 4 and 5

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- create table mysql
+CREATE TABLE IF NOT EXISTS first_table (
+       id INT,	    
+       name VARCHAR(256)
+	  );
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- print description of table
+describe first_table;

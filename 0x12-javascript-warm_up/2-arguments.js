28a10d3042a58e422e45a870322734747e0af64f
juan-gimenez <juancitogimenez0@gmail.com>
2022-04-04 10:05:42 -0700

    semistandad checked

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const a = process.argv.length;
 
 if (a === 3) {
-    console.log('Argument found');
-} else if (a <= 2)
-    console.log('No argument');
-else {
-    console.log('Arguments found');
+  console.log('Argument found');
+} else if (a <= 2) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
 }

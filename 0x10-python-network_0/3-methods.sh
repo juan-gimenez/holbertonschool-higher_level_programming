8b9e1ef85996c2a050dc75f837a83f86c0280b30
juan-gimenez <juancitogimenez0@gmail.com>
2022-04-22 12:38:02 -0700

    changed line numb

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# displays all HTTP methods the server will accept
-# using allow option
+# displays all HTTP methods the server will accept using allow option
 curl -s -I "$1" | grep "Allow" | cut -d' ' -f2-

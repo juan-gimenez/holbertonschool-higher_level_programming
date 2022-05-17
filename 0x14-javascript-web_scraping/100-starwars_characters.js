8b8af884912fbf571b4312b244fde89353093b7e
juan-gimenez <juancitogimenez0@gmail.com>
2022-05-17 16:52:18 -0700

    task adv characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const r = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+r(url, function (err, response, body) {
+    if (err) console.log(err);
+    else {
+	const characters = JSON.parse(body).characters;
+	characters.forEach((character) => {
+	    r(character, function (err, response, body) {
+		if (err) console.log(err);
+		else console.log(JSON.parse(body).name);
+	    });
+	});
+    }
+});

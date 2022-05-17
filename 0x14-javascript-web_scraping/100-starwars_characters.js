9d8cdaa013c47c979305ef52d4882aa389404c18
juan-gimenez <juancitogimenez0@gmail.com>
2022-05-17 16:54:28 -0700

    semistandart

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,14 +4,14 @@ const r = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 r(url, function (err, response, body) {
-    if (err) console.log(err);
-    else {
-	const characters = JSON.parse(body).characters;
-	characters.forEach((character) => {
-	    r(character, function (err, response, body) {
-		if (err) console.log(err);
-		else console.log(JSON.parse(body).name);
-	    });
-	});
-    }
+  if (err) console.log(err);
+  else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      r(character, function (err, response, body) {
+        if (err) console.log(err);
+        else console.log(JSON.parse(body).name);
+      });
+    });
+  }
 });

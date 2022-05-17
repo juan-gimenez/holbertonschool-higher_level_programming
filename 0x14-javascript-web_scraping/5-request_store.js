13feaf407d23b24130ccf1486f4492fad557de38
juan-gimenez <juancitogimenez0@gmail.com>
2022-05-17 15:43:12 -0700

    task 5 get content to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
+      if (error) console.log(error);
+    });
+  }
+});
